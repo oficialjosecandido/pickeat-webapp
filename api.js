0de7c38c5fbadc078603ada99069e07242f71707
JOSE CANDIDO <jose.candido_ext@a-to-be.com>
2025-02-18 19:46:43 +0000

    update timeslots

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const baseURL =
   "https://pickeat-production.azurewebsites.net/";
+  // "https://pickeat-backend.azurewebsites.net/"
 
 // const baseURL = "http://192.168.1.241:8080/";
 
